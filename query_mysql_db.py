751d98b85bc2da3072522546948cf993a2d5e473
pedromoisescamacho <pedromoisescamacho@gmail.com>
2025-02-18 14:38:03 -0800

    test connection function

--- query_mysql_db.py
+++ query_mysql_db.py
@@ -5,6 +5,23 @@ def create_connection_string(db_config):
     """db_config needs to have user, password, host, and database name"""
     return f"mysql+mysqlconnector://{db_config['user']}:{db_config['password']}@{db_config['host']}/{db_config['database']}"
 
+def test_connection(db_dict_config):
+    """
+    Test the connection to the database using the provided credentials.
+    db_dict_config is a dict with keys user, password, host, and database.
+    """
+    connection_string = create_connection_string(db_dict_config)
+    engine = create_engine(connection_string)
+    
+    try:
+        with engine.connect() as connection:
+            print("Connection successful")
+            message = 'connection successful'
+    except Exception as e:
+        print(f"Connection failed: {e}")
+        message = 'connection failed'
+    return message
+
 def query_db(query, db_dict_config):
     """
     Fetch data from the database using the provided SQL query.
